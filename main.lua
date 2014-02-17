0e9f43ffa0d90bcbf15288a954fa4f60b9a808e0
Yujan Shrestha <mydevelopergames@gmail.com>
2014-02-16 20:12:40 -0600

    add help api

---
+++ main.lua
@@ -0,0 +1,20 @@
+---Returns the list of locations the API will look in for help files.
+function help.path() 
+    return ""
+end
+
+---Sets the list of locations the API will look in for help files.
+function help.setPath(path) 
+    path = ""    
+end
+
+---Returns the path to the file containing the help page for topic, or nil if the topic cannot be found.
+function help.lookup(topic)
+    topic = {}
+    return ""
+end
+
+---Returns a list of all available help topics.
+function help.topics() 
+    return {}
+end
