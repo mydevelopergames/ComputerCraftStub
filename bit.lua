7fcbaf40628701555d580c790602754e044c8301
Yujan Shrestha <mydevelopergames@gmail.com>
2014-02-16 18:56:26 -0600

    Add bit.lua

---
+++ bit.lua
@@ -0,0 +1,70 @@
+bit = {}
+
+--- Shifts a number left by a specified number of bits.
+-- @param n @class number
+-- @param bits @class number
+-- @return @class number
+function bit.blshift( n,  bits)
+    
+end
+--- Shifts a number right arithmetically by a specified number of bits.
+function bit.brshift( n,  bits)
+    n = 1
+    bits = 1
+    return 1
+end
+
+--- Shifts a number right logically by a specified number of bits.
+-- @param n @class number
+-- @param bits @class number
+-- @return @class number
+function bit.blogic_rshift( n,  bits)
+    
+end
+
+--- Computes the bitwise exclusive OR of two numbers.
+-- @param m @class number
+-- @param n @class number
+-- @return @class number
+function bit.bxor( m,  n)
+    
+end
+
+--- Computes the bitwise OR of two numbers.
+-- @param m @class number
+-- @param n @class number
+-- @return @class number
+function bit.bor( m,  n)
+    
+end
+
+--- Computes the bitwise AND of two numbers.
+-- @param m @class number
+-- @param n @class number
+-- @return @class number
+function bit.band( m,  n)
+    
+end
+
+--- Computes the bitwise NOT of a numbers.
+-- @param n @class number
+-- @return @class number
+function bit.bnot( n)
+    
+end
+
+--- @deprecated
+-- Converts a number to an array (numerically-indexed table) containing the corresponding binary bit values.
+-- @param n @class number
+-- @return @class table
+function bit.tobits( n)
+    
+end
+
+--- @deprecated
+-- Converts an array (numerically-indexed table) of 0 and 1 values representing a number in binary into that number.
+-- @param bit_tbl @class table
+-- @return @class number
+function bit.tonumb(bit_tbl)
+    
+end
